374b638a0d362e6b01699e372b035dac48c721f6
Johannes Wanzek <j.wanzek@enbw.com>
2023-01-19 12:17:08 +0100

    Clowd.Squirrel .net 7 WPF Test

---
+++ SqTest/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using NuGet.Versioning;
+using Squirrel;
+
+namespace SqTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // run Squirrel first, as the app may exit after these run
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: OnAppInstall,
+                onAppUninstall: OnAppUninstall,
+                onEveryRun: OnAppRun);
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
+        }
+    }
+}
---
+++ SqTest/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Squirrel;
+
+namespace SqTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UpdateMyApp().ConfigureAwait(false);
+        }
+
+        private async Task UpdateMyApp()
+        {
+            // I used ./publish and ./releases folders in project dir
+            // change accordingly
+            using var mgr = new UpdateManager(@"C:\Users\Johannes\RiderProjects\SqTest\SqTest\releases");
+            var newVersion = await mgr.UpdateApp();
+
+            // optionally restart the app automatically, or ask the user if/when they want to restart
+            if (newVersion != null)
+            {
+                var settings = new MetroDialogSettings()
+                {
+                    AffirmativeButtonText = "Update now",
+                    NegativeButtonText = "Remind me later",
+                    //DialogButtonFontSize = 20D
+                };
+
+                MessageDialogResult result = await this.ShowMessageAsync("New Update available",
+                    "There is a new update available for ASI!" + SquirrelRuntimeInfo.EntryExePath,
+                    MessageDialogStyle.AffirmativeAndNegative,
+                    settings);
+
+                if (result == MessageDialogResult.Affirmative)
+                {
+                    UpdateManager.RestartApp();
+                }
+            }
+        }
+    }
+}
